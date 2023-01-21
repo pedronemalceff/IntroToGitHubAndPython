05ce90c9c64c23718870877c7dddc96e50a30197
pedronemalceff <54723731+pedronemalceff@users.noreply.github.com>
2023-01-20 19:54:39 -0500

    adding code for homework

--- src/main/intro_to_python.py
+++ src/main/intro_to_python.py
@@ -1,1 +1,16 @@
+import numpy as np
+print('Question 1')
+matrix = np.eye(3)
+print(matrix)
+print()
+
+print('Question 2')
+matrix[matrix == 0] += 3
+print(matrix)
+print()
+
+print('Question 3')
+matrix = np.delete(matrix, -1, axis=1)
+print(matrix)
+
 
